25149e069d86a4d5659407e3890c19a0143cf8c4
e.aleynikov <on1xas509@gmail.com>
2023-06-27 17:07:58 +0300

    init db

--- state_control/bot.py
+++ state_control/bot.py
@@ -1,8 +1,11 @@
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.redis import RedisStorage
 from redis.asyncio.client import Redis
+from asyncpg import connect
+import asyncpg
 
 from handlers.handlers import state_router
+from middleware.init_user_db import InitUsersMeddleware
 
 TOKEN = "6095290433:AAE6t3nVcf9nNGOKdqVLZwobmy7hJuy-DWI"
 
@@ -12,6 +15,8 @@ storage_redis: RedisStorage = RedisStorage(redis=redis, state_ttl=15)
 
 bot: Bot = Bot(TOKEN)
 dp: Dispatcher = Dispatcher(storage=storage_redis)
+pool = asyncpg.create_pool(user='topevgn', password='1234', host='localhost', database='bot', command_timeout=60)
+dp.update.outer_middleware(InitUsersMeddleware(pool))
 
 
 dp.include_router(state_router)
